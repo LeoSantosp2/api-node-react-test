cbea5069245fb5791534d88428510de0f26a4365
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2024-11-25 21:30:00 -0300

    Adicionado as rotas de mostrar apenas um funcionário, editar e excluir um funcionário

--- src/routes/employees-route.ts
+++ src/routes/employees-route.ts
@@ -5,6 +5,9 @@ import employeesController from '../controllers/employees-controller';
 const router = Router();
 
 router.get('/', employeesController.index);
+router.get('/:id', employeesController.show);
 router.post('/', employeesController.store);
+router.put('/:id', employeesController.update);
+router.delete('/:id', employeesController.delete);
 
 export default router;
