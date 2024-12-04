885031ef109759a0b63b37e98791daf1c1dc9b93
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2024-12-04 18:18:16 -0300

    Criado arquivo com as rotas de books

---
+++ src/routes/books-route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import booksController from '../controllers/books-controller';
+
+const router = Router();
+
+router.get('/', booksController.index);
+router.post('/', booksController.store);
+router.put('/:id', booksController.update);
+router.delete('/:id', booksController.delete);
+
+export default router;
