55ea8013b6374c2ef4b382526b0ac4a95a966f94
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2024-12-04 18:17:31 -0300

    Adicionado mais dois nomes de parâmetros

--- src/interfaces/request-props.ts
+++ src/interfaces/request-props.ts
@@ -3,6 +3,8 @@ import { Request } from 'express';
 export interface RequestProps<T = undefined> extends Request {
   params: {
     id: string;
+    bookId: string;
+    copyCode: string;
   };
 
   body: T;
