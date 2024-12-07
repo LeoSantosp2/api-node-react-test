56a9b0a45eec9462ebe8f35a62bc53ad0e4a8ac9
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2024-12-07 15:03:27 -0300

    Criado arquivo direcionado as rotas de CRUD

---
+++ src/routes/clients-route.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import clientsController from '../controllers/clients-controller';
+
+const router = Router();
+
+router.get('/', clientsController.index);
+router.get('/:id', clientsController.show);
+router.post('/', clientsController.store);
+router.put('/:id', clientsController.update);
+router.delete('/:id', clientsController.delete);
+
+export default router;
