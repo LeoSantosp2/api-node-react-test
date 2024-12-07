d619092d5aaf27a40911e604838bd7d122cdeb44
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2024-12-07 15:03:07 -0300

    Criado interface para tirar o body de clientes

---
+++ src/interfaces/clients-body-props.ts
@@ -0,0 +1,12 @@
+export interface ClientsBodyProps {
+  name: string;
+  cpf: string;
+  date_of_birth: string;
+  street_address: string;
+  number_address: string;
+  complement_address: string;
+  neighborhood_address: string;
+  city_address: string;
+  state_address: string;
+  zipcode_address: string;
+}
