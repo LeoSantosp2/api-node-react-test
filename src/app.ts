54a457d5a7515b06c0b7157fc705b1c41a9ffc8d
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2024-12-07 16:25:16 -0300

    Feito alteração

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import 'dotenv/config';
 
 import employeesRouter from './routes/employees-route';
 import booksRouter from './routes/books-route';
-import booksCopyRouter from './routes/books-copy-router';
+import booksCopyRouter from './routes/books-copy-route';
 import clientsRouter from './routes/clients-route';
+import rentedBooksRouter from './routes/rented-books-route';
 
 class App {
   public app: express.Application;
@@ -25,6 +26,7 @@ class App {
     this.app.use('/books', booksRouter);
     this.app.use('/books-copy', booksCopyRouter);
     this.app.use('/clients', clientsRouter);
+    this.app.use('/rented-books', rentedBooksRouter);
   }
 }
 
