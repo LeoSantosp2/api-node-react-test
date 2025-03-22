de1ea4360a779ffebca7f816971ac4aa36be923f
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-03-22 19:34:08 -0300

    Adicionado condições para buscar os livros alugados

--- src/controllers/rented-books-controller.ts
+++ src/controllers/rented-books-controller.ts
@@ -5,13 +5,223 @@ import prisma from '../config/prisma';
 
 import { RequestProps } from '../interfaces/request-props';
 import { RentedBooksBodyProps } from '../interfaces/rented-books-body-props';
+import { RentedBooksProps } from '../interfaces/rented-books-props';
 
 class RentedBooksController {
   async index(req: RequestProps, res: Response) {
     try {
-      const rentedBooks = await prisma.rentedBooks.findMany();
+      const { q, page, limit, status, withdrawal } = req.query;
 
-      return res.status(200).json(rentedBooks);
+      let rentedBooks: RentedBooksProps[] = [];
+      let total = 0;
+
+      const client = await prisma.clients.findFirst({
+        select: { id: true, name: true },
+        where: {
+          OR: [{ cpf: q ? q : '' }, { name: q ? { contains: q } : '' }],
+        },
+      });
+
+      const book = await prisma.books.findFirst({
+        select: {
+          id: true,
+          title: true,
+          BooksCopy: { select: { book_id: true, copy_code: true } },
+        },
+        where: { title: q ? { contains: q } : '' },
+      });
+
+      // Data de Retirada AND Nome do Cliente/CPF
+      if (withdrawal && client) {
+        rentedBooks = await prisma.rentedBooks.findMany({
+          skip: Number(page),
+          take: Number(limit),
+          orderBy: { client: { name: 'asc' } },
+          select: {
+            id: true,
+            rent_date: true,
+            limit_date: true,
+            status: true,
+            client: { select: { name: true } },
+            books_copy: {
+              select: { copy_code: true, book: { select: { title: true } } },
+            },
+          },
+          where: {
+            AND: [
+              { rent_date: new Date(withdrawal) },
+              {
+                client: {
+                  OR: [{ cpf: q }, { name: { contains: q } }],
+                },
+              },
+              { status },
+            ],
+          },
+        });
+
+        total = await prisma.rentedBooks.count({
+          where: {
+            AND: [
+              { rent_date: new Date(withdrawal) },
+              {
+                client: {
+                  OR: [{ cpf: q }, { name: { contains: q } }],
+                },
+              },
+              { status },
+            ],
+          },
+        });
+
+        return res.status(200).json({ data: rentedBooks, total: total });
+      }
+
+      // Data de Retirada AND Títutlo do Livro
+      if (withdrawal && book) {
+        rentedBooks = await prisma.rentedBooks.findMany({
+          skip: Number(page),
+          take: Number(limit),
+          orderBy: { client: { name: 'asc' } },
+          select: {
+            id: true,
+            rent_date: true,
+            limit_date: true,
+            status: true,
+            client: { select: { name: true } },
+            books_copy: {
+              select: { copy_code: true, book: { select: { title: true } } },
+            },
+          },
+          where: {
+            AND: [
+              { rent_date: new Date(withdrawal) },
+              {
+                books_copy: { book: { title: { contains: q } } },
+              },
+              { status },
+            ],
+          },
+        });
+
+        total = await prisma.rentedBooks.count({
+          where: {
+            AND: [
+              { rent_date: new Date(withdrawal) },
+              {
+                books_copy: { book: { title: { contains: q } } },
+              },
+              { status },
+            ],
+          },
+        });
+
+        return res.status(200).json({ data: rentedBooks, total: total });
+      }
+
+      if (client) {
+        rentedBooks = await prisma.rentedBooks.findMany({
+          skip: Number(page),
+          take: Number(limit),
+          orderBy: { client: { name: 'asc' } },
+          select: {
+            id: true,
+            rent_date: true,
+            limit_date: true,
+            status: true,
+            client: { select: { name: true } },
+            books_copy: {
+              select: { copy_code: true, book: { select: { title: true } } },
+            },
+          },
+          where: { client_id: client.id, status },
+        });
+
+        total = await prisma.rentedBooks.count({
+          where: { client_id: client.id, status },
+        });
+
+        return res.status(200).json({ data: rentedBooks, total: total });
+      }
+
+      if (book) {
+        rentedBooks = await prisma.rentedBooks.findMany({
+          skip: Number(page),
+          take: Number(limit),
+          orderBy: { client: { name: 'asc' } },
+          select: {
+            id: true,
+            rent_date: true,
+            limit_date: true,
+            status: true,
+            client: { select: { name: true } },
+            books_copy: {
+              select: { copy_code: true, book: { select: { title: true } } },
+            },
+          },
+          where: {
+            books_copy: { book: { title: { contains: book.title } } },
+            status,
+          },
+        });
+
+        total = await prisma.rentedBooks.count({
+          where: {
+            books_copy: { book: { title: { contains: book.title } }, status },
+            status,
+          },
+        });
+
+        return res.status(200).json({ data: rentedBooks, total: total });
+      }
+
+      if (withdrawal) {
+        rentedBooks = await prisma.rentedBooks.findMany({
+          skip: Number(page),
+          take: Number(limit),
+          orderBy: { client: { name: 'asc' } },
+          select: {
+            id: true,
+            rent_date: true,
+            limit_date: true,
+            status: true,
+            client: { select: { name: true } },
+            books_copy: {
+              select: { copy_code: true, book: { select: { title: true } } },
+            },
+          },
+          where: { rent_date: new Date(withdrawal), status },
+        });
+
+        total = await prisma.rentedBooks.count({
+          where: { rent_date: new Date(withdrawal), status },
+        });
+
+        return res.status(200).json({ data: rentedBooks, total: total });
+      }
+
+      rentedBooks = await prisma.rentedBooks.findMany({
+        skip: Number(page),
+        take: Number(limit),
+        orderBy: { client: { name: 'asc' } },
+        select: {
+          id: true,
+          rent_date: true,
+          limit_date: true,
+          status: true,
+          client: { select: { name: true } },
+          books_copy: {
+            select: { copy_code: true, book: { select: { title: true } } },
+          },
+        },
+        where: { status },
+      });
+
+      total = await prisma.rentedBooks.count({
+        where: { status },
+      });
+
+      return res.status(200).json({ data: rentedBooks, total: total });
     } catch (err) {
       return res.status(400).json({ error: (err as Error).message });
     }
@@ -62,6 +272,25 @@ class RentedBooksController {
         return res.status(400).json({ error: 'Cópia já está alugada.' });
       }
 
+      const clientWasLateTwice = await prisma.rentedBooks.findMany({
+        select: {
+          id: true,
+          client_id: true,
+          late: true,
+        },
+
+        where: {
+          AND: [{ client_id: client.id }, { late: 1 }],
+        },
+      });
+
+      if (clientWasLateTwice.length >= 2) {
+        return res.status(400).json({
+          error:
+            'Cliente atrasou a devolução 2x ou mais. Cliente não pode mais alugar livros.',
+        });
+      }
+
       const newRentedBook = {
         id: v4(),
         client_id: client.id,
@@ -86,7 +315,7 @@ class RentedBooksController {
   async update(req: RequestProps<RentedBooksBodyProps>, res: Response) {
     try {
       const { id } = req.params;
-      const { status } = req.body;
+      const { status, late } = req.body;
       const { copyCode } = req.query;
 
       if (!status) {
@@ -104,7 +333,7 @@ class RentedBooksController {
       }
 
       await prisma.rentedBooks.update({
-        data: { return_date: new Date(), status },
+        data: { return_date: new Date(), status, late },
         where: { id },
       });
 
