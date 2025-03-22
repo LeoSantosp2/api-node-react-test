e8dbad199ab260c8130c2ab4fd6c531663e0f191
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-03-22 19:35:07 -0300

    Realizado alteração

--- src/routes/rented-books-route.ts
+++ src/routes/rented-books-route.ts
@@ -4,8 +4,8 @@ import rentedBooksController from '../controllers/rented-books-controller';
 
 const router = Router();
 
-router.get('/', rentedBooksController.index);
-router.get('/:clientId', rentedBooksController.show);
+router.get('/?', rentedBooksController.index);
+// router.get('/:clientId', rentedBooksController.show);
 router.post('/', rentedBooksController.store);
 router.put('/:id', rentedBooksController.update);
 
